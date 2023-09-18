cb9525a2ce0edbe0f8d3d710ae19788d12b7cf29
Ivan <ivandolgolaptev@gmail.com>
2023-09-18 22:57:29 +0300

    init: start prod

---
+++ src/main/java/servlet/RegistrationServlet.java
@@ -0,0 +1,9 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.net.HttpRetryException;
+
+@WebServlet("/reg")
+public class RegistrationServlet extends HttpServlet {
+}
